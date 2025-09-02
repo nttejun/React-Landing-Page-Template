dbb79e55483afd975b0f69232bcecbae671c8a7b
nttejun <nttejun@gmail.com>
2025-09-02 16:15:49 +0900

    법인 신청 폼 추가

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { Gallery } from "./components/gallery";
 import { Testimonials } from "./components/testimonials";
 import { Team } from "./components/Team";
 import { Contact } from "./components/contact";
+import { BizContact } from "./components/bizcontact";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
@@ -34,6 +35,7 @@ const App = () => {
       <Testimonials data={landingPageData.Testimonials} />
       <Team data={landingPageData.Team} />
       <Contact data={landingPageData.Contact} />
+      <BizContact data={landingPageData.BizContact} />
     </div>
   );
 };
